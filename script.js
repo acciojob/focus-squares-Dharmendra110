12d01a8645d96428dcf0ce3ad999390fe728b98b
Dharmendra Mahilang <161195208+Dharmendra110@users.noreply.github.com>
2025-12-13 22:12:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-//your JS code here. If required.
-const squares = document.querySelectorAll(".square");
+let squares = document.querySelectorAll(".square");
 
 squares.forEach((square) => {
-    square.addEventListener("mouseover", () => {
-        squares.forEach((sq) => {
-            if (sq !== square) {
-                sq.style.backgroundColor = "#6F4E37"; // coffee
-            } else {
-                sq.style.backgroundColor = "#e6e6fa"; // lavender
-            }
-        });
+  square.addEventListener("mouseover", () => {
+    squares.forEach((s) => {
+      if (s === square) {
+        s.style.backgroundColor = "#E6E6FA"; // lavender
+      } else {
+        s.style.backgroundColor = "#6F4E37"; // coffee
+      }
     });
+  });
 });
